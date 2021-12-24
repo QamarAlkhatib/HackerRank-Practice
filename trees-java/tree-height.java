3f58330fd706b3df45ccb7fbf04f275b888e1ce5
Qamar <qalkhatib0@gmail.com>
2021-12-24 21:10:37 +0200

    adding trees Hackerrank practice

---
+++ trees-java/tree-height.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static int height(Node root) {
+      int left_len = 0;
+      int right_len = 0;
+      
+      if(root == null){
+          return 0;
+      }
+      if(root.left != null){
+          left_len = height(root.left) +1;
+      }
+      if(root.right != null){
+          right_len = height(root.right) +1;
+      }
+      
+      if (left_len > right_len){
+          return left_len;
+      }
+      else{
+          return right_len;
+      }
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        int height = height(root);
+        System.out.println(height);
+    }	
+}
